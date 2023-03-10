8f6a6b94a733108a70236e8e351c924b85a41a8b
Anvesa Jaiswal <anvesa_telus@fico.com>
2023-03-10 19:02:57 +0000

    Renegotiate PARR Files modified - [CreatesParr,ParrDetails]

--- src/main/webapp/pages/CreatesParr/CreatesParr.js
+++ src/main/webapp/pages/CreatesParr/CreatesParr.js
@@ -20,5 +20,32 @@ Partial.onReady = function() {
      * e.g. to get value of text widget named 'username' use following script
      * 'Partial.Widgets.username.datavalue'
      */
+    Partial.Variables.installmentBANCreateParr.dataSet = [];
 
+};
+Partial.button4Click = function($event, widget) {
+    debugger;
+    //selectBanParrTable1
+};
+Partial.button8_1Click = function($event, widget) {
+    debugger;
+};
+Partial.button10_1Click = function($event, widget) {
+    debugger;
+};
+
+Partial.button4Click = function($event, widget) {
+    debugger;
+    Partial.Variables.installmentBANCreateParr.dataSet = [];
+    //BANName
+    Partial.Widgets.selectBanParrTable1.selectedItems.forEach(function(a) {
+
+        Partial.obj = {
+            "BAN": a.BAN,
+            "BANName": a.BANName
+        }
+        Partial.Variables.installmentBANCreateParr.dataSet.push(Partial.obj);
+    })
+
+    Partial.Widgets.dialog1.close();
 };
