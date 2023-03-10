f136b8d582eb0015aff952781e8eb9c25413ed58
drakshayani konduru <drakshayanikonduru_telus@fico.com>
2023-03-10 13:34:36 +0000

    create Parr binding New files added - [PaymentArrangement] Files modified - [CreateParr,debug.configuration.json,pom.xml,project-services.xml]

--- src/main/webapp/pages/CreateParr/CreateParr.js
+++ src/main/webapp/pages/CreateParr/CreateParr.js
@@ -10,7 +10,7 @@
  */
 
 /* perform any action on widgets/variables within this block */
-Partial.onReady = function () {
+Partial.onReady = function() {
     /*
      * variables can be accessed through 'Partial.Variables' property here
      * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -20,4 +20,37 @@ Partial.onReady = function () {
      * e.g. to get value of text widget named 'username' use following script
      * 'Partial.Widgets.username.datavalue'
      */
+    Partial.Variables.InstallmentOption.dataSet.datavalue = 'Number of Installments';
+};
+
+Partial.createInstalmntScheduleClick = function($event, widget) {
+
+    var installmentSchedule = new Array();
+    var amount = Partial.Widgets.ParrTotal.datavalue / Partial.Variables.NoOfInstallments.dataSet.datavalue;
+    for (var i = 0; i < Partial.Variables.NoOfInstallments.dataSet.datavalue; i++) {
+
+        var collectionPaymentInstallment = {};
+        collectionPaymentInstallment.sequenceId = i;
+        var tempDate = new Date();
+        collectionPaymentInstallment.date = new Date(tempDate.setMonth(tempDate.getMonth() + i));
+        collectionPaymentInstallment.amount = amount;
+        if (i == 0) {
+            collectionPaymentInstallment.cummPmtAmount = collectionPaymentInstallment.amount;
+        } else {
+            collectionPaymentInstallment.cummPmtAmount = installmentSchedule[i - 1].amount + collectionPaymentInstallment.amount;
+        }
+        alert("CummAmount : " + collectionPaymentInstallment.cummPmtAmount);
+        installmentSchedule.push(collectionPaymentInstallment);
+        alert("installmentSchedule length : " +
+            installmentSchedule.length);
+    }
+    alert("Schedule size :" + installmentSchedule.length);
+    Partial.Variables.ParrInstallmentSchedule.dataSet.push(...installmentSchedule);
+    alert("ParrInstallmentSchedule size :" + Partial.Variables.ParrInstallmentSchedule.length);
+
+};
+
+Partial.noOfInstlmntChange = function($event, widget, newVal, oldVal) {
+
+    Partial.Variables.NoOfInstallments.dataSet.datavalue = newVal;
 };
