cebfa383c4d222c955b68cf0c8a9256ef79eccd7
Yamini Sharma <yaminisharma_telus@fico.com>
2023-04-19 10:19:21 +0000

    Parr list UI changes. Files modified - [CreateParr,ParrList]

--- src/main/webapp/pages/CreateParr/CreateParr.js
+++ src/main/webapp/pages/CreateParr/CreateParr.js
@@ -21,88 +21,99 @@ Partial.onReady = function() {
      * 'Partial.Widgets.username.datavalue'
      */
     //Partial.Widgets.InstallmentOptions.dataValue = 'NoOfInstallments';
+    App.Variables.errorMsg.dataSet.dataValue = null;
 };
 
 Partial.createInstalmntScheduleClick = function($event, widget) {
+    App.Variables.errorMsg.dataSet.dataValue = null;
+    debugger;
 
-    document.getElementById("cancelButton").style.display = "none";
+    if (Partial.Widgets.ParrTotal.dataValue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "ParrTotal is mandatory";
+    } else if (Partial.Widgets.RecurrenceDropdown.dataValue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Recurrence is mandatory";
+    } else if (Partial.Widgets.InstallmentOptionRadio.dataValue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Installment type is mandatory";
+    } else {
 
-    var installmentSchedule = new Array();
-    var amount = '';
-    var remainder = 0;
-    var installmentSize = '';
-    var totalInstallmentAmt = 0;
-    //Psuedo code start
-    //check if numOfInstallmetn is selected 
-    if (Partial.Widgets.InstallmentOptionRadio.datavalue == 'NoOfInstallments') {
-        installmentSize = Partial.Variables.NoOfInstallments.dataSet.datavalue;
-        amount = Partial.Widgets.ParrTotal.datavalue / Partial.Variables.NoOfInstallments.dataSet.datavalue;
+        document.getElementById("cancelButton").style.display = "none";
+
+        var installmentSchedule = new Array();
+        var amount = '';
+        var remainder = 0;
+        var installmentSize = '';
+        var totalInstallmentAmt = 0;
+        //Psuedo code start
+        //check if numOfInstallmetn is selected 
+        if (Partial.Widgets.InstallmentOptionRadio.datavalue == 'NoOfInstallments') {
+            installmentSize = Partial.Variables.NoOfInstallments.dataSet.datavalue;
+            amount = Partial.Widgets.ParrTotal.datavalue / Partial.Variables.NoOfInstallments.dataSet.datavalue;
+        }
+        //check if amtPerInstallment is selected 
+        if (Partial.Widgets.InstallmentOptionRadio.datavalue == 'AmtPerInstallment') {
+            remainder = Partial.Widgets.ParrTotal.datavalue % Partial.Variables.AmountPerInstallment.dataSet.dataValue;
+            installmentSize = parseInt(Partial.Widgets.ParrTotal.datavalue / Partial.Variables.AmountPerInstallment.dataSet.dataValue);
+            amount = Partial.Variables.AmountPerInstallment.dataSet.dataValue;
+            var lastInstallmentAmount = 0;
+            if (remainder !== 0) {
+                installmentSize = installmentSize + 1;
+                lastInstallmentAmount = remainder;
+                //input logic to add remainder into the last row of the collection PaymentInstallment, if it exists 
+            }
+            //ask abou installment Schedule size limitations 
+        }
+        // //Psuedo code End
+        for (var i = 0; i < installmentSize; i++) {
+
+            var collectionPaymentInstallment = {};
+            collectionPaymentInstallment.sequenceId = i + 1;
+            var tempDate = '';
+            if (i == installmentSize - 1 && remainder !== 0) {
+                amount = lastInstallmentAmount;
+            }
+            if (i > 0) {
+                tempDate = new Date(installmentSchedule[i - 1].date);
+            } else {
+                tempDate = new Date();
+            }
+            if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Weekly') {
+
+                tempDate = new Date(tempDate.setDate(tempDate.getDate() + 7));
+                collectionPaymentInstallment.date = new Date(tempDate);
+                // collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
+            } else if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Bi-Weekly') {
+
+                tempDate = new Date(tempDate.setDate(tempDate.getDate() + 15));
+                collectionPaymentInstallment.date = new Date(tempDate);
+                //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
+            } else if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Monthly') {
+
+                tempDate = new Date(tempDate.setMonth(tempDate.getMonth() + 1));
+                collectionPaymentInstallment.date = new Date(tempDate);
+                //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
+            } else {
+
+                tempDate = new Date(tempDate.setMonth(tempDate.getMonth() + 1));
+                collectionPaymentInstallment.date = new Date(tempDate);
+                //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
+            }
+
+            collectionPaymentInstallment.amount = amount;
+            if (i === 0) {
+                //collectionPaymentInstallment.cummPmtAmount = collectionPaymentInstallment.amount;
+            } else {
+                //collectionPaymentInstallment.cummPmtAmount = installmentSchedule[i - 1].amount + collectionPaymentInstallment.amount;
+            }
+            totalInstallmentAmt = totalInstallmentAmt + amount;
+
+            installmentSchedule.push(collectionPaymentInstallment);
+        }
+        Partial.AmtOverUnder = Partial.Widgets.ParrTotal.datavalue - totalInstallmentAmt;
+        Partial.Variables.isCreateScheduleClicked.dataSet.datavalue = 'true';
+        Partial.Variables.ParrInstallmentSchedule.dataSet.splice(0, Partial.Variables.ParrInstallmentSchedule.dataSet.length);
+        Partial.Variables.ParrInstallmentSchedule.dataSet.push(...installmentSchedule);
+        Variables.createPaymentArrangement.dataSet.installments.dataset.push(...installmentSchedule);
     }
-    //check if amtPerInstallment is selected 
-    if (Partial.Widgets.InstallmentOptionRadio.datavalue == 'AmtPerInstallment') {
-        remainder = Partial.Widgets.ParrTotal.datavalue % Partial.Variables.AmountPerInstallment.dataSet.dataValue;
-        installmentSize = parseInt(Partial.Widgets.ParrTotal.datavalue / Partial.Variables.AmountPerInstallment.dataSet.dataValue);
-        amount = Partial.Variables.AmountPerInstallment.dataSet.dataValue;
-        var lastInstallmentAmount = 0;
-        if (remainder !== 0) {
-            installmentSize = installmentSize + 1;
-            lastInstallmentAmount = remainder;
-            //input logic to add remainder into the last row of the collection PaymentInstallment, if it exists 
-        }
-        //ask abou installment Schedule size limitations 
-    }
-    // //Psuedo code End
-    for (var i = 0; i < installmentSize; i++) {
-
-        var collectionPaymentInstallment = {};
-        collectionPaymentInstallment.sequenceId = i + 1;
-        var tempDate = '';
-        if (i == installmentSize - 1 && remainder !== 0) {
-            amount = lastInstallmentAmount;
-        }
-        if (i > 0) {
-            tempDate = new Date(installmentSchedule[i - 1].date);
-        } else {
-            tempDate = new Date();
-        }
-        if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Weekly') {
-
-            tempDate = new Date(tempDate.setDate(tempDate.getDate() + 7));
-            collectionPaymentInstallment.date = new Date(tempDate);
-            // collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
-        } else if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Bi-Weekly') {
-
-            tempDate = new Date(tempDate.setDate(tempDate.getDate() + 15));
-            collectionPaymentInstallment.date = new Date(tempDate);
-            //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
-        } else if (Partial.Widgets.RecurrenceDropdown.datavalue == 'Monthly') {
-
-            tempDate = new Date(tempDate.setMonth(tempDate.getMonth() + 1));
-            collectionPaymentInstallment.date = new Date(tempDate);
-            //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
-        } else {
-
-            tempDate = new Date(tempDate.setMonth(tempDate.getMonth() + 1));
-            collectionPaymentInstallment.date = new Date(tempDate);
-            //collectionPaymentInstallment.date = tempDate.getMonth() + "/" + tempDate.getDate() + "/" + tempDate.getFullYear();
-        }
-
-        collectionPaymentInstallment.amount = amount;
-        if (i === 0) {
-            //collectionPaymentInstallment.cummPmtAmount = collectionPaymentInstallment.amount;
-        } else {
-            //collectionPaymentInstallment.cummPmtAmount = installmentSchedule[i - 1].amount + collectionPaymentInstallment.amount;
-        }
-        totalInstallmentAmt = totalInstallmentAmt + amount;
-
-        installmentSchedule.push(collectionPaymentInstallment);
-    }
-    Partial.AmtOverUnder = Partial.Widgets.ParrTotal.datavalue - totalInstallmentAmt;
-    Partial.Variables.isCreateScheduleClicked.dataSet.datavalue = 'true';
-    Partial.Variables.ParrInstallmentSchedule.dataSet.splice(0, Partial.Variables.ParrInstallmentSchedule.dataSet.length);
-    Partial.Variables.ParrInstallmentSchedule.dataSet.push(...installmentSchedule);
-    Variables.createPaymentArrangement.dataSet.installments.dataset.push(...installmentSchedule);
-    //document.getElementById("cancelButton").style.display = "block";
 };
 
 Partial.noOfInstlmntChange = function($event, widget, newVal, oldVal) {
