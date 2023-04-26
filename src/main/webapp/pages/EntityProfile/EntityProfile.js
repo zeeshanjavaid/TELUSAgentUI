f3fdbba517102275ccfd483559da49b84751aaa0
Ravi Kumar <ravik_telus@fico.com>
2023-04-26 06:26:19 +0000

    User note fix and Entity profile cancel ban Files modified - [EntityProfile,Lookup]

--- src/main/webapp/pages/EntityProfile/EntityProfile.js
+++ src/main/webapp/pages/EntityProfile/EntityProfile.js
@@ -68,76 +68,15 @@ Partial.button3Click = function($event, widget) {
 
     }
 
-
-    // const message = document.getElementById("p01");
-    // message.innerHTML = "";
-
-    // if (Partial.Widgets.textarea1.datavalue == undefined || Partial.Widgets.textarea1.datavalue == "") {
-    //     try {
-    //         isError = true;
-    //         throw "Please enter the note"
-    //     } catch (err) {
-    //         message.innerHTML = err;
-    //     }
-    // } else {
-    //     var banId = Partial.Widgets.getEntityDetailsTable1_1.selecteditem.banId;
-    //     var note = Partial.Widgets.textarea1.datavalue;
-    //     var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
-
-    //     Partial.Variables.saveNote.setInput({
-
-    //         'entityId': entityId,
-    //         'banId': banId,
-    //         'notes': note,
-    //         'docId': documentId
-
-    //     });
-
-
-
-    //     Partial.Variables.saveNote.invoke({},
-    //         function(data) {
-    //             // Success Callback
-    //             debugger;
-    //             console.log("success", data);
-
-    //         },
-    //         function(error) {
-    //             // Error Callback
-    //             console.log("error", error)
-
-    //         });
-
-    //     Partial.Variables.saveDocument.invoke({},
-    //         function(data) {
-    //             // Success Callback
-    //             debugger;
-    //             console.log("success", data);
-    //             documentId = data.id;
-
-    //         },
-    //         function(error) {
-    //             // Error Callback
-    //             console.log("error", error)
-
-    //         });
-
-
-
-
-
-    //     // Partial.Variables.saveNote.invoke();
-    // }
-
-
 };
 
 
 
 Partial.button3Click1 = function($event, widget) {
 
+    //for file upload
     debugger;
-
+    documentId = "";
     const message = document.getElementById("p01");
     message.innerHTML = "";
 
@@ -149,92 +88,193 @@ Partial.button3Click1 = function($event, widget) {
             message.innerHTML = err;
         }
     } else {
-        var banId = Partial.Widgets.getEntityDetailsTable1_1.selecteditem.banId;
-        var note = Partial.Widgets.textarea1.datavalue;
-        var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
+        if (Partial.Widgets.fileupload1.selectedFiles != undefined || Partial.Widgets.fileupload1.selectedFiles != "") {
+            var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
 
-        Partial.Variables.saveNote.setInput({
+            var checkDocument = Partial.Variables.checkIfDocIsPresent.dataSet.length;
 
-            'entityId': entityId,
-            'banId': banId,
-            'notes': note,
-            'docId': documentId
+            if (checkDocument == 0) {
 
-        });
+                var docName = Partial.Widgets.fileupload1.selectedFiles[0].name;
 
+                Partial.Variables.saveDocument.setInput({
 
-
-        Partial.Variables.saveNote.invoke({},
-            function(data) {
-                // Success Callback
-                debugger;
-                console.log("success", data);
-                documentId = "";
-
-            },
-            function(error) {
-                // Error Callback
-                console.log("error", error)
-
-            });
+                    'documentName': docName,
+                    'document': Partial.Widgets.fileupload1.selectedFiles
+                });
 
 
 
+                Partial.Variables.saveDocument.invoke({},
+                    function(data) {
+                        // Success Callback
+                        debugger;
+                        console.log("success", data);
+                        documentId = data.id;
+
+                        //for note save
+                        const message = document.getElementById("p01");
+                        message.innerHTML = "";
+
+                        if (Partial.Widgets.textarea1.datavalue == undefined || Partial.Widgets.textarea1.datavalue == "") {
+                            try {
+                                isError = true;
+                                throw "Please enter the note"
+                            } catch (err) {
+                                message.innerHTML = err;
+                            }
+                        } else {
+                            var banId = Partial.Widgets.getEntityDetailsTable1_1.selecteditem.banId;
+                            var note = Partial.Widgets.textarea1.datavalue;
+                            var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
+
+                            Partial.Variables.saveNote.setInput({
+
+                                'entityId': entityId,
+                                'banId': banId,
+                                'notes': note,
+                                'docId': documentId
+
+                            });
 
 
-        // Partial.Variables.saveNote.invoke();
+
+                            Partial.Variables.saveNote.invoke({},
+                                function(data) {
+                                    // Success Callback
+                                    debugger;
+                                    console.log("success", data);
+                                    documentId = "";
+                                    // App.pageRefresh();
+                                    window.location.reload();
+
+                                },
+                                function(error) {
+                                    // Error Callback
+                                    console.log("error", error)
+
+                                });
+
+                        }
+
+
+
+                    },
+                    function(error) {
+                        // Error Callback
+                        console.log("error", error)
+
+                    });
+
+            } else {
+                const message1 = document.getElementById("p02");
+                message1.innerHTML = "Attachment already exists with entityId: " + entityId;
+            }
+        } else {
+
+
+            //for note save
+            const message = document.getElementById("p01");
+            message.innerHTML = "";
+
+            if (Partial.Widgets.textarea1.datavalue == undefined || Partial.Widgets.textarea1.datavalue == "") {
+                try {
+                    isError = true;
+                    throw "Please enter the note"
+                } catch (err) {
+                    message.innerHTML = err;
+                }
+            } else {
+                var banId = Partial.Widgets.getEntityDetailsTable1_1.selecteditem.banId;
+                var note = Partial.Widgets.textarea1.datavalue;
+                var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
+
+                Partial.Variables.saveNote.setInput({
+
+                    'entityId': entityId,
+                    'banId': banId,
+                    'notes': note,
+                    'docId': documentId
+
+                });
+
+
+
+                Partial.Variables.saveNote.invoke({},
+                    function(data) {
+                        // Success Callback
+                        debugger;
+                        console.log("success", data);
+                        documentId = "";
+                        // App.pageRefresh();
+                        window.location.reload();
+
+                    },
+                    function(error) {
+                        // Error Callback
+                        console.log("error", error)
+
+                    });
+
+            }
+
+        }
     }
-
-
 
 };
 
 Partial.anchor1Click = function($event, widget) {
 
-    debugger;
 
     Partial.Widgets.getEntityDetailsTable1_1.selecteditem = false;
 
 };
-Partial.fileupload1Select = function($event, widget, selectedFiles) {
+// Partial.fileupload1Select = function($event, widget, selectedFiles) {
 
-    debugger;
-    documentId = "";
-    var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
+//     debugger;
+//     documentId = "";
+//     var entityId = Partial.Widgets.getEntityDetailsTable1_1.dataset[0].entityId;
 
-    var checkDocument = Partial.Variables.checkIfDocIsPresent.dataSet.length;
+//     var checkDocument = Partial.Variables.checkIfDocIsPresent.dataSet.length;
 
-    if (checkDocument == 0) {
+//     if (checkDocument == 0) {
 
-        var docName = Partial.Widgets.fileupload1.selectedFiles[0].name;
+//         var docName = Partial.Widgets.fileupload1.selectedFiles[0].name;
 
-        Partial.Variables.saveDocument.setInput({
+//         Partial.Variables.saveDocument.setInput({
 
-            'documentName': docName,
-            'document': Partial.Widgets.fileupload1.selectedFiles
-        });
+//             'documentName': docName,
+//             'document': Partial.Widgets.fileupload1.selectedFiles
+//         });
 
-        Partial.Variables.saveDocument.invoke({},
-            function(data) {
-                // Success Callback
-                debugger;
-                console.log("success", data);
-                documentId = data.id;
+//         Partial.Variables.saveDocument.invoke({},
+//             function(data) {
+//                 // Success Callback
+//                 debugger;
+//                 console.log("success", data);
+//                 documentId = data.id;
 
-            },
-            function(error) {
-                // Error Callback
-                console.log("error", error)
+//             },
+//             function(error) {
+//                 // Error Callback
+//                 console.log("error", error)
 
-            });
+//             });
 
-    } else {
-        const message1 = document.getElementById("p02");
-        message1.innerHTML = "Attachment already exists with entityId: " + entityId;
-    }
-
-
+//     } else {
+//         const message1 = document.getElementById("p02");
+//         message1.innerHTML = "Attachment already exists with entityId: " + entityId;
+//     }
 
 
+
+
+
+// };
+Partial.button4_1Click = function($event, widget) {
+
+    Partial.Widgets.fileupload1.selectedFiles = "";
+    const message1 = document.getElementById("p02");
+    message1.innerHTML = "";
 
 };
