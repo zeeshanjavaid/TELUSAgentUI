343223d5e681d0ba2f463ff34011aa51389282fd
Sachin Balgi <sachinbalgi_telus@fico.com>
2023-05-11 13:45:56 +0000

    New Java service created for EntityBanTravelHistory New files added - [EntityBanTravelHistoryService] Files modified - [debug.configuration.json,pom.xml,project-services.xml]

---
+++ services/EntityBanTravelHistoryService/src/com/fico/dmp/entitybantravelhistoryservice/EntityBanTravelHistoryService.java
@@ -0,0 +1,57 @@
+/*Copyright (c) 2020-2021 fico.com All Rights Reserved.
+ This software is the confidential and proprietary information of fico.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with fico.com*/
+package com.fico.dmp.entitybantravelhistoryservice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.wavemaker.runtime.util.logging.FAWBStaticLoggerBinder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import com.wavemaker.runtime.service.annotations.HideFromClient;
+
+//import com.fico.dmp.entitybantravelhistoryservice.model.*;
+
+/**
+ * This is a singleton class with all its public methods exposed as REST APIs via generated controller class.
+ * To avoid exposing an API for a particular public method, annotate it with @HideFromClient.
+ *
+ * Method names will play a major role in defining the Http Method for the generated APIs. For example, a method name
+ * that starts with delete/remove, will make the API exposed as Http Method "DELETE".
+ *
+ * Method Parameters of type primitives (including java.lang.String) will be exposed as Query Parameters &
+ * Complex Types/Objects will become part of the Request body in the generated API.
+ */
+@ExposeToClient
+public class EntityBanTravelHistoryService {
+
+    private static final Logger logger = FAWBStaticLoggerBinder.getSingleton().getLoggerFactory().getLogger(EntityBanTravelHistoryService.class.getName());
+
+    @Autowired
+    private SecurityService securityService;
+
+    /**
+     * This is sample java operation that accepts an input from the caller and responds with "Hello".
+     *
+     * SecurityService that is Autowired will provide access to the security context of the caller. It has methods like isAuthenticated(),
+     * getUserName() and getUserId() etc which returns the information based on the caller context.
+     *
+     * Methods in this class can declare HttpServletRequest, HttpServletResponse as input parameters to access the
+     * caller's request/response objects respectively. These parameters will be injected when request is made (during API invocation).
+     */
+    public String getEntityBanTravelHistoryService(String name, HttpServletRequest request) {
+        logger.debug("Starting sample operation with request url " + request.getRequestURL().toString());
+        
+        String result = name;
+       
+        logger.debug("Returning {}", result);
+        return result;
+    }
+
+}
