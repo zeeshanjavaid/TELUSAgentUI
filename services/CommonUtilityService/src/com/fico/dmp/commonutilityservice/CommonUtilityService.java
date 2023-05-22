f4e3fe13d9a34265f1dcd3b0c995ddea9026ada8
Sachin Balgi <sachinbalgi_telus@fico.com>
2023-05-22 14:29:39 +0000

    Added CommonUtilityService to get the logged in user teamId and modified New files added - [CommonUtilityService] Files modified - [EntityHistoryActionList,debug.configuration.json,pom.xml,project-services.xml]

---
+++ services/CommonUtilityService/src/com/fico/dmp/commonutilityservice/CommonUtilityService.java
@@ -0,0 +1,73 @@
+/*Copyright (c) 2020-2021 fico.com All Rights Reserved.
+ This software is the confidential and proprietary information of fico.com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with fico.com*/
+package com.fico.dmp.commonutilityservice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.wavemaker.runtime.util.logging.FAWBStaticLoggerBinder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.fico.dmp.telusagentuidb.TeamUser;
+import com.fico.dmp.telusagentuidb.service.TeamUserService;
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import com.wavemaker.runtime.service.annotations.HideFromClient;
+
+//import com.fico.dmp.commonutilityservice.model.*;
+
+/**
+ * This is a singleton class with all its public methods exposed as REST APIs via generated controller class.
+ * To avoid exposing an API for a particular public method, annotate it with @HideFromClient.
+ *
+ * Method names will play a major role in defining the Http Method for the generated APIs. For example, a method name
+ * that starts with delete/remove, will make the API exposed as Http Method "DELETE".
+ *
+ * Method Parameters of type primitives (including java.lang.String) will be exposed as Query Parameters &
+ * Complex Types/Objects will become part of the Request body in the generated API.
+ */
+@ExposeToClient
+public class CommonUtilityService {
+
+    private static final Logger logger = FAWBStaticLoggerBinder.getSingleton().getLoggerFactory().getLogger(CommonUtilityService.class.getName());
+
+    @Autowired
+    private SecurityService securityService;
+    
+    @Autowired
+    private TeamUserService teamUserService;
+
+    /**
+     * This is sample java operation that accepts an input from the caller and responds with "Hello".
+     *
+     * SecurityService that is Autowired will provide access to the security context of the caller. It has methods like isAuthenticated(),
+     * getUserName() and getUserId() etc which returns the information based on the caller context.
+     *
+     * Methods in this class can declare HttpServletRequest, HttpServletResponse as input parameters to access the
+     * caller's request/response objects respectively. These parameters will be injected when request is made (during API invocation).
+     */
+    public String getLoggedInUserTeamId(Integer userId) {
+        logger.info("Inside CommonUtilityService#getLoggerInUserTeamId " + userId);
+        
+        String query = "userId="+ userId + " order by updatedOn desc";
+       // Pageable pageable = 
+        Pageable pageable = PageRequest.of(0, 1);
+        Page<TeamUser> teamUserPageList = teamUserService.findAll(query, pageable);
+        TeamUser teamUser = null;
+        String teamId = null;
+        if(!teamUserPageList.isEmpty()) {
+        	teamUser = teamUserPageList.getContent().get(0);
+        	teamId = teamUser.getTeam().getTeamId();
+        }
+        
+        logger.info("teamId-----"+teamId);
+        return teamId;
+    }
+
+}
