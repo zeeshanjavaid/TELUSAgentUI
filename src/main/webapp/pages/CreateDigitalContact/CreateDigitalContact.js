29aa9a0ad307990d7f0ccc758a6b078ab5de9893
Yamini Sharma <yaminisharma_telus@fico.com>
2023-06-13 07:25:32 +0000

    Validation added for create contact screen. Files modified - [Contact,CreateDigitalContact,UpdateDigitalContact]

--- src/main/webapp/pages/CreateDigitalContact/CreateDigitalContact.js
+++ src/main/webapp/pages/CreateDigitalContact/CreateDigitalContact.js
@@ -22,7 +22,64 @@ Partial.onReady = function() {
      */
 };
 
-Partial.CancelClick = function($event, widget) {
+function messageTimeout() {
+    App.Variables.successMessage.dataSet.dataValue = null;
+    App.Variables.errorMsg.dataSet.dataValue = null;
+}
 
+function validateEmail(email) {
+    return email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+};
+
+Partial.CancelClick = function($event, widget) {
     Partial.Variables.ContactPageName.dataSet.dataValue = 'Contact';
+    App.Variables.errorMsg.dataSet.dataValue = null;
+
+    // clearing the fields
+    Partial.Widgets.TITLESelect.datavalue = "";
+    Partial.Widgets.firstName.datavalue = "";
+    Partial.Widgets.lastName.datavalue = "";
+    Partial.Widgets.emailText.datavalue = "";
+    Partial.Widgets.work.datavalue = "";
+    Partial.Widgets.cellPhone.datavalue = "";
+    Partial.Widgets.ext.datavalue = "";
+    Partial.Widgets.fax.datavalue = "";
+    Partial.Widgets.comments.datavalue = "";
+};
+
+
+Partial.createContact = function($event, widget) {
+    if (Partial.Widgets.TELUSContactsSelect.datavalue == "" || Partial.Widgets.TELUSContactsSelect.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Telus Contact is mandatory";
+    } else if (Partial.Widgets.EmailForNoticesSelect.datavalue == "" || Partial.Widgets.EmailForNoticesSelect.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Email for Notices is mandatory";
+    } else if (Partial.Widgets.firstName.datavalue == "" || Partial.Widgets.firstName.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "First Name is mandatory";
+    } else if (Partial.Widgets.lastName.datavalue == "" || Partial.Widgets.lastName.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Last Name is mandatory";
+    } else {
+        if (Partial.Widgets.emailText._datavalue == "" || Partial.Widgets.emailText._datavalue == undefined || validateEmail(Partial.Widgets.emailText._datavalue)) {
+            // API Call will come here
+            App.Variables.successMessage.dataSet.dataValue = "Contact created successfully.";
+            Partial.Widgets.TITLESelect.datavalue = "";
+            Partial.Widgets.firstName.datavalue = "";
+            Partial.Widgets.lastName.datavalue = "";
+            Partial.Widgets.emailText.datavalue = "";
+            Partial.Widgets.work.datavalue = "";
+            Partial.Widgets.cellPhone.datavalue = "";
+            Partial.Widgets.ext.datavalue = "";
+            Partial.Widgets.fax.datavalue = "";
+            Partial.Widgets.comments.datavalue = "";
+            Partial.Variables.ContactPageName.dataSet.dataValue = 'Contact';
+        }
+
+
+        if (!Partial.Widgets.emailText._datavalue == "" || !Partial.Widgets.emailText._datavalue == undefined) {
+            if (!validateEmail(Partial.Widgets.emailText._datavalue)) {
+                App.Variables.errorMsg.dataSet.dataValue = "Please enter valid Email Address";
+            }
+        }
+    }
+
+    setTimeout(messageTimeout, 8000);
 };
