f6f8a84ab5d98b14f4d0ba08f19720d4121b934a
Yamini Sharma <yaminisharma_telus@fico.com>
2023-06-16 09:26:15 +0000

    Order desk view updated. Files modified - [OrderDeskAgentView]

--- src/main/webapp/pages/OrderDeskAgentView/OrderDeskAgentView.js
+++ src/main/webapp/pages/OrderDeskAgentView/OrderDeskAgentView.js
@@ -10,7 +10,8 @@
  */
 
 /* perform any action on widgets/variables within this block */
-Page.onReady = function () {
+Page.onReady = function() {
+    debugger;
     /*
      * variables can be accessed through 'Page.Variables' property here
      * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -20,4 +21,5 @@ Page.onReady = function () {
      * e.g. to get value of text widget named 'username' use following script
      * 'Page.Widgets.username.datavalue'
      */
+    Page.Variables.getCollectionTreatment_orderDeskView.dataSet;
 };
