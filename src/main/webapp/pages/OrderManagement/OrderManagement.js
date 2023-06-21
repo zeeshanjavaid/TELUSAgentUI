4ae6874f17463429f4e5f4c9a2937a5b33933c45
Ravi Kumar <ravik_telus@fico.com>
2023-06-21 09:20:34 +0000

    Integrated telus API with create sus Files modified - [EntityHistoryActionList,OrderManagement,CollectionTreatmentService,debug.configuration.json]

--- src/main/webapp/pages/OrderManagement/OrderManagement.js
+++ src/main/webapp/pages/OrderManagement/OrderManagement.js
@@ -43,11 +43,6 @@ Partial.CreateRestoralRequestClick = function($event, widget) {
 
     Partial.Widgets.CreateRestoralRequestdialog.open();
 };
-Partial.CreateCeaseRequestClick = function($event, widget) {
-    Partial.Widgets.OrderPopOver.hidePopover();
-    Partial.Variables.UserLoggedInVar.dataSet.dataValue = App.Variables.getLoggedInUserDetails.dataSet.emplId;
-    Partial.Widgets.CreateCeaseRequestdialog.open();
-}
 
 Partial.closeCreateSuspensionActionDialog = function() {
     Partial.Variables.errorMsg.dataSet.dataValue = "";
@@ -62,9 +57,41 @@ Partial.closeCreateRestoralActionDialog = function() {
 
     Partial.Widgets.CreateRestoralRequestdialog.close();
 };
-Partial.closeCreateCeaseActionDialogClick = function($event, widget) {
-    Partial.Variables.errorMsg.dataSet.dataValue = "";
-    Partial.Variables.UserLoggedInVar.dataSet.dataValue = App.Variables.getLoggedInUserDetails.dataSet.emplId;
+Partial.createbuttonClick = function($event, widget) {
 
-    Partial.Widgets.CreateCeaseRequestdialog.close();
+    debugger;
+
+    if (Partial.Widgets.reasonCodeSelect.datavalue == "" || Partial.Widgets.reasonCodeSelect.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Reason code is mandatory";
+    } else if (Partial.Widgets.actionStatusSelect.datavalue == "" || Partial.Widgets.actionStatusSelect.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Status is mandatory";
+    } else if (Partial.Widgets.prioritySelect.datavalue == "" || Partial.Widgets.prioritySelect.datavalue == undefined) {
+        App.Variables.errorMsg.dataSet.dataValue = "Priority is mandatory";
+    } else {
+        // API Call will come here
+
+        Partial.Variables.createOrderManagment.setInput({
+            "CollectionTreatmentStepCreate": {
+                'stepTypeCode': "SUSPEND",
+                'reasonCode': Partial.Widgets.reasonCodeSelect.datavalue,
+                'stepDate': Partial.Widgets.dueDate.datavalue,
+                'comment': Partial.Widgets.Comment.datavalue,
+                'status': Partial.Widgets.actionStatusSelect.datavalue,
+                'priority': Partial.Widgets.prioritySelect.datavalue,
+                'assignedAgentId': Partial.Widgets.assignedPersonSelect.datavalue,
+                'assignedTeam': Partial.Widgets.assignedTeamSelect.datavalue,
+                'collectionTreatment': {
+                    'id': 0
+                },
+                'channel': {
+                    'channelOrgId': "string",
+                    'userId': Partial.Variables.loggedInUser.dataSet.id
+                },
+            },
+        });
+        Partial.Variables.createOrderManagment.invoke();
+        App.Variables.successMessage.dataSet.dataValue = "Action Suspended successfully.";
+        Partial.Widgets.CreateSuspensionRequestdialog.close();
+    }
+
 };
