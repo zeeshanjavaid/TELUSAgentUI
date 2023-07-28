afd65db716a9b57bcff08fd09a252e753f6f6b6e
Yamini Sharma <yaminisharma_telus@fico.com>
2023-07-28 06:46:29 +0000

    Updated final changes for home screen around filter grid dropdowns. Files modified - [Home]

--- src/main/webapp/pages/Home/Home.js
+++ src/main/webapp/pages/Home/Home.js
@@ -22,7 +22,6 @@ Page.onReady = function() {
      * 'Page.Widgets.username.datavalue'
      */
 
-    debugger;
     Page.Variables.errorMsg.dataSet.dataValue = '';
     Page.Variables.UserLoggedInVar_home.dataSet.empId = App.Variables.getLoggedInUserDetails.dataSet.emplId;
     Page.Widgets.AssignedTeamSelectEV.datavalue = "ALL";
@@ -178,9 +177,10 @@ Page.banViewTable_OnRowexpand = function($event, widget, row, $data) {
     App.showRowExpansionBanViewHome(row, $data);
 };
 
+// assigned team Onchange for EV
 Page.assignedTeamSelectEV_onChange = function($event, widget, newVal, oldVal) {
     if (Page.Widgets.AssignedTeamSelectEV.datavalue == 'ALL') {
-        Page.Variables.getAllActiveUserList_HomeEV.invoke();
+        Page.Variables.getAllActiveUserList_HomeEV_forALL.invoke();
     } else {
         Page.Variables.getUserListByTeamId_homeEV.setInput({
             'teamId': Page.Widgets.AssignedTeamSelectEV.datavalue
@@ -189,9 +189,10 @@ Page.assignedTeamSelectEV_onChange = function($event, widget, newVal, oldVal) {
     }
 };
 
+// assigned team Onchange for BV
 Page.assignedTeamSelectBV_onChange = function($event, widget, newVal, oldVal) {
     if (Page.Widgets.AssignedTeamSelectBV.datavalue == 'ALL') {
-        Page.Variables.getAllActiveUserList_HomeBV.invoke();
+        Page.Variables.getAllActiveUserList_HomeBV_forALL.invoke();
     } else {
         Page.Variables.getUserListByTeamId_homeBV.setInput({
             'teamId': Page.Widgets.AssignedTeamSelectBV.datavalue
@@ -199,43 +200,6 @@ Page.assignedTeamSelectBV_onChange = function($event, widget, newVal, oldVal) {
         Page.Variables.getUserListByTeamId_homeBV.invoke();
     }
 };
-
-Page.entityOwnerEVSelectOn_Change = function($event, widget, newVal, oldVal) {
-    if (Page.Widgets.entityOwnerSelectEV.datavalue == 'ALL') {
-        Page.Variables.workCategorySelect_HomeEV.invoke();
-    } else if (Page.Widgets.entityOwnerSelectEV.datavalue == 'NULL') {
-        Page.Variables.workcategoriesByEmpId_homeEV.setInput({
-            'emplId': App.Variables.getLoggedInUserDetails.dataSet.emplId
-        });
-        Page.Variables.workcategoriesByEmpId_homeEV.invoke();
-    } else {
-        Page.Variables.workcategoriesByEmpId_homeEV.setInput({
-            'emplId': Page.Widgets.entityOwnerSelectEV.datavalue
-        });
-        Page.Variables.workcategoriesByEmpId_homeEV.invoke();
-    }
-};
-
-Page.entityOwnerBVSelectOn_Change = function($event, widget, newVal, oldVal) {
-    if (Page.Widgets.entityOwnerSelectBV.datavalue == 'ALL') {
-        Page.Variables.workCategorySelect_HomeBV.invoke();
-    } else if (Page.Widgets.entityOwnerSelectBV.datavalue == 'NULL') {
-        Page.Variables.workcategoriesByEmpId_homeBV.setInput({
-            'emplId': App.Variables.getLoggedInUserDetails.dataSet.emplId
-        });
-        Page.Variables.workcategoriesByEmpId_homeBV.invoke();
-    } else {
-        Page.Variables.workcategoriesByEmpId_homeBV.setInput({
-            'emplId': Page.Widgets.entityOwnerSelectBV.datavalue
-        });
-        Page.Variables.workcategoriesByEmpId_homeBV.invoke();
-    }
-};
-
-Page.workCategorySelectEVChange = function($event, widget, newVal, oldVal) {};
-
-Page.workCategorySelectBVChange = function($event, widget, newVal, oldVal) {};
-
 
 // adding 'All' in the dropdown list for assignedTeam dropdown for ENTITY VIEW
 Page.getTeamList_HomeEVonSuccess = function(variable, data) {
@@ -263,6 +227,7 @@ Page.getTeamList_HomeBVonSuccess = function(variable, data) {
 
 // adding 'All' in the dropdown list for entityOwner dropdown for ENTITY VIEW
 Page.getAllActiveUserList_HomeEVonSuccess = function(variable, data) {
+    debugger;
     if (Page.Variables.getAllActiveUserList_HomeEV.dataSet.length > 1) {
         Page.Variables.getAllActiveUserList_HomeEV.dataSet.unshift({
             empId: 'ALL',
@@ -295,21 +260,6 @@ Page.workCategoryValues_HomeBVonSuccess = function(variable, data) {
     Page.Variables.workCategoryValues_HomeBV.dataSet = [];
     Page.Variables.workCategoryValues_HomeBV.dataSet = data;
 };
-
-
-Page.workCategorySelect_HomeEVonSuccess = function(variable, data) {
-    if (Page.Widgets.entityOwnerSelectEV.datavalue == 'ALL') {
-        Page.Variables.workCategoryValues_HomeEV.dataSet = data;
-    }
-};
-
-
-Page.workCategorySelect_HomeBVonSuccess = function(variable, data) {
-    if (Page.Widgets.entityOwnerSelectBV.datavalue == 'ALL') {
-        Page.Variables.workCategoryValues_HomeBV.dataSet = data;
-    }
-};
-
 
 Page.getUserListByTeamId_homeEVonSuccess = function(variable, data) {
     Page.Variables.getAllActiveUserList_HomeEV.dataSet = data;
@@ -399,11 +349,27 @@ function workCategoryData(item, index) {
     workCategoryDataArray.push(item.code);
 };
 
-Page.CollectionDataServiceGetAssignedEntitiesInEntityView3onSuccess = function(variable, data){
-	
+
+Page.getAllActiveUserList_HomeEV_forALLonSuccess = function(variable, data) {
+    if (Page.Variables.getAllActiveUserList_HomeEV_forALL.dataSet.length > 1) {
+        Page.Variables.getAllActiveUserList_HomeEV_forALL.dataSet.unshift({
+            empId: 'ALL',
+            firstName: 'ALL',
+            lastName: ''
+        });
+        Page.Variables.getAllActiveUserList_HomeEV.dataSet = data;
+        Page.Widgets.entityOwnerSelectEV.datavalue = Page.Variables.getAllActiveUserList_HomeEV_forALL.dataSet[0].empId;
+    }
 };
 
-
-Page.CollectionDataServiceGetAssignedEntitiesInEntityView3onError = function(variable, data, xhrObj){
-	
+Page.getAllActiveUserList_HomeBV_forALLonSuccess = function(variable, data) {
+    if (Page.Variables.getAllActiveUserList_HomeBV_forALL.dataSet.length > 1) {
+        Page.Variables.getAllActiveUserList_HomeBV_forALL.dataSet.unshift({
+            empId: 'ALL',
+            firstName: 'ALL',
+            lastName: ''
+        });
+        Page.Variables.getAllActiveUserList_HomeBV.dataSet = data;
+        Page.Widgets.entityOwnerSelectBV.datavalue = Page.Variables.getAllActiveUserList_HomeBV_forALL.dataSet[0].empId;
+    }
 };
