345a886d7b337d3fa4031f3baa4a2d0af83b55ba
Ravi Kumar <ravik_telus@fico.com>
2023-09-26 08:40:00 +0000

    fixed loggeding user Files modified - [Home]

--- src/main/webapp/pages/Home/Home.js
+++ src/main/webapp/pages/Home/Home.js
@@ -465,7 +465,7 @@ Page.workcategoriesByEmpId_homeEVonSuccess = function(variable, data) {
 
 
     Page.Variables.CollectionDataServiceGetAssignedEntitiesInEntityView3.setInput({
-        'entityOwner': 10, //App.Variables.getLoggedInUserDetails.dataSet.emplId,
+        'entityOwner': App.Variables.getLoggedInUserDetails.dataSet.emplId,
         'workCategory': finalWCentityview,
         /*'portfolio': Page.Variables.portfolioEntityView_home.dataSet[0].dataValue,
         'billingSystem': Page.Variables.billingSystemEntityView_home.dataSet[0].dataValue,
@@ -522,7 +522,7 @@ Page.workcategoriesByEmpId_homeBVonSuccess = function(variable, data) {
 
     // api call to display data in table for ban view
     Page.Variables.CollectionDataServiceGetassignedEntitiesInClassicView2.setInput({
-        'entityOwner': 10, // App.Variables.getLoggedInUserDetails.dataSet.emplId,
+        'entityOwner': App.Variables.getLoggedInUserDetails.dataSet.emplId,
         'workCategory': finalWCbanview,
         /*'portfolio': Page.Variables.portfolioEntityView_home.dataSet[0].dataValue,
         'billingSystem': Page.Variables.billingSystemEntityView_home.dataSet[0].dataValue,
