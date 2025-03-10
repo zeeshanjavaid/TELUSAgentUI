b4b6537a679e65a64ab91d3b82f012e2fc2e9cca
Zeeshan Javaid <zeeshan.javaid@telus.com>
2025-03-10 21:53:54 +0000

    Addtional requirements for 239 Files modified - [ActDetails,ActivityLogRowExpansion]

--- src/main/webapp/pages/ActivityLogRowExpansion/ActivityLogRowExpansion.js
+++ src/main/webapp/pages/ActivityLogRowExpansion/ActivityLogRowExpansion.js
@@ -29,15 +29,119 @@ Partial.onReady = function() {
             'relatedBusinessEntityId': row.relatedBusinessEntityId
         };
 
-        // Add another attribute if row.id is 1
-        if (row.relatedBusinessEntitySubType == "NOTICE") {
+        /*if (row.relatedBusinessEntitySubType == "NOTICE") {
             inputParams.businessEntityEventType = "CCSResponse";
-        }
+        }*/
 
         Partial.Variables.getActivityLogPerRef.setInput(inputParams);
         Partial.Variables.getActivityLogPerRef.invoke();
     }
 };
+Partial.expandedRowDataTable1Beforedatarender = function(widget, $data, $columns) {
+    //control table column show or hide here
+    debugger;
+    $data.forEach((item, index) => {
+        console.log(`Index: ${index}, relatedBusinessEntitySubType:`, item.relatedBusinessEntitySubType);
+        console.log(`Index: ${index}, relatedBusinessEntityStatus:`, item.relatedBusinessEntityStatus);
+        console.log(`Index: ${index}, businessEntityEventType:`, item.businessEntityEventType);
+
+        if (item.relatedBusinessEntitySubType.toUpperCase() == 'NOTC' || item.relatedBusinessEntitySubType.toUpperCase() == 'NOTICE') {
+            //$columns.type.show = false;
+            item.comment = formatTextWithLineBreaks(extractCharacteristics(item));
+            if (item.businessEntityEventType && item.businessEntityEventType.toUpperCase() == 'CCSRESPONSE' && item.additionalCharacteristics && Array.isArray(item.additionalCharacteristics)) {
+                //item.deliveryType = getFicoClientCodeDescription(item.additionalCharacteristics.find(item => item.name === 'CLIENT_CODE').value);
+                var clientCodeCharacteristic = item.additionalCharacteristics.find(function(char) {
+                    return char.name === 'CLIENT_CODE';
+                });
+
+                var deviceUsedCharacteristic = item.additionalCharacteristics.find(function(char) {
+                    return char.name === 'DEVICE_USED';
+                });
+                var codeDescription = '';
+                var deviceUsed = '';
+                if (clientCodeCharacteristic && clientCodeCharacteristic.value) {
+                    codeDescription = getFicoClientCodeDescription(clientCodeCharacteristic.value);
+                }
+                item.deliveryType = codeDescription;
+                if (deviceUsedCharacteristic && deviceUsedCharacteristic.value) {
+                    item.deliveryType = item.deliveryType + deviceUsedCharacteristic.value;
+                }
+            }
+            if (item.businessEntityEventType && item.businessEntityEventType.toUpperCase() == 'STATUS' && item.additionalCharacteristics && Array.isArray(item.additionalCharacteristics)) {
+                debugger;
+                item.lang = getLanguage(item.additionalCharacteristics);
+            }
+            debugger;
+            $columns.deliveryType.show = true;
+            $columns.lang.show = true;
+            $columns.comment.show = true;
+            $columns.commentLabel.show = false;
+        }
+
+        if (item.relatedBusinessEntitySubType.toUpperCase() == 'CALL-OB') {
+            $columns.dueDate.show = true;
+            item.dueDate = item.relatedBusinessEntityDueDate;
+            $columns.phoneNumber.show = true;
+            item.phoneNumber = item.additionalCharacteristics.find(item => item.name === 'PhoneNumber').value;
+            $columns.reachedCustomer.show = true;
+            item.reachedCustomer = item.additionalCharacteristics.find(item => item.name === 'ReachedCustomer').value;
+            $columns.relatedBusinessEntityAssignedTo.show = true;
+            $columns.relatedBusinessEntityAssignedTeam.show = true;
+        }
+
+        if (item.relatedBusinessEntitySubType.toUpperCase() == 'CALL-IB' || item.relatedBusinessEntitySubType.toUpperCase() == 'SUSPEND' ||
+            item.relatedBusinessEntitySubType.toUpperCase() == 'RESTORE' || item.relatedBusinessEntitySubType.toUpperCase() == 'CEASE') {
+            $columns.dueDate.show = true;
+            item.dueDate = item.relatedBusinessEntityDueDate;
+            $columns.relatedBusinessEntityAssignedTo.show = true;
+            $columns.relatedBusinessEntityAssignedTeam.show = true;
+        }
+        /*if (item.additionalCharacteristics != undefined) {
+            item.additionalCharacteristics.forEach(char => {
+                if (char.name === 'cssRequest') {
+                    const emails = [];
+                    debugger;
+                    Object.keys(char.value).forEach(key => {
+                        if (key.startsWith("Contact_") && key.endsWith("_Email")) {
+                            console.log(`${key}:`, char.value[key]);
+                            emails.push(char.value[key]); // Collect emails
+                        }
+                    });
+                    if (emails.length > 0) {
+                        console.log("Concatenated Emails:\n" + emails.join("\n"));
+                        $columns.email.show = true;
+                        item.email = emails.join("\n");
+                    }
+                }
+                if (char.name === 'cssResponse') {
+                    debugger;
+                    console.log("Message:", char.value.message);
+                }
+                if (char.name === 'EmailAddress') {
+                    //                if (item.relatedBusinessEntitySubType === 'EM-IN' && char.name === 'EmailAddress') {
+                    debugger;
+                    console.log("Email Address:", char.value);
+                    $columns.email.show = true;
+                    item.email = char.value; // + ' ' + index;
+                }
+                if (item.relatedBusinessEntitySubType === 'CALL-OB' && char.name === 'PhoneNumber') {
+                    debugger;
+                    console.log("Phone Number:", char.value);
+                    $columns.phoneNumber.show = true;
+                    item.phoneNumber = char.value; // + ' ' + index;
+                }
+            });
+        }*/
+    });
+    debugger;
+    /* Ensure columns exist
+    if ($columns) {
+        $columns.type.show = !$data.some(row => row.relatedBusinessEntitySubType === 'CALL-OB');
+    } else {
+        console.error("Columns not found.");
+    }*/
+};
+
 
 Partial.popoverClicked = function(comment) {
     console.log("Popover clicked with comment:", comment);
@@ -49,6 +153,25 @@ Partial.getActivityLogPerRefonSuccess = function(variable, data) {
     debugger;
     //change data or set values here
 };
+
+function getLanguage(jsonArray) {
+    debugger;
+    // Find the object with name "cssRequest"
+    let cssRequestObj = jsonArray.find(item => item.name === "cssRequest");
+
+    if (!cssRequestObj || !cssRequestObj.value) {
+        console.log("cssRequest object not found or has no value.");
+        return "";
+    }
+
+    if (!cssRequestObj.value.Language) {
+        console.log("Language key missing in cssRequest value.");
+        return "";
+    }
+
+    console.log("Language found:", cssRequestObj.value.Language);
+    return cssRequestObj.value.Language;
+}
 
 function getFirstAvailableContactMediumType(data) {
     for (var i = 0; i < data.additionalCharacteristics.length; i++) {
@@ -155,20 +278,20 @@ function extractCharacteristics(jsonData) {
 function getFicoClientCodeDescription(code) {
     const ficoCollectionsMap = {
         "ES1": "excluded from contact",
-        "TNS": "text message sent successfully to @SMS_Phone",
-        "TIU": "text message delivery to @SMS_Phone failed",
-        "TST": "inbound text message opted-out - @SMS_Phone",
+        "TNS": "text message sent successfully to ",
+        "TIU": "text message delivery to ",
+        "TST": "inbound text message opted-out ",
         "TCS": "inbound text message - already paid",
         "TFS": "inbound text message - pay now",
-        "EIS": "email sent @To_Email_Address",
-        "ERS": "email delivery to @To_Email_Address failed",
+        "EIS": "email sent to ",
+        "ERS": "email delivery to ",
         "ERW": "email - no contact window available",
         "EQ1": "email already paid link clicked",
         "EQ2": "email pay now link clicked",
         "C01": "web channel - already paid selected",
         "C10": "web channel - external link selected",
         "NNA": "outbound call - no answer",
-        "AVA": "outbound call - left voicemail",
+        "AVA": "outbound call - left voicemail ",
         "WII": "outbound call answered",
         "XRS": "outbound call - further assistance selected",
         "CCP": "outbound call - already paid selected",
@@ -182,88 +305,6 @@ function getFicoClientCodeDescription(code) {
 
     return ficoCollectionsMap[code] || "";
 }
-Partial.expandedRowDataTable1Beforedatarender = function(widget, $data, $columns) {
-    //control table column show or hide here
-    debugger;
-    $data.forEach((item, index) => {
-        console.log(`Index: ${index}, relatedBusinessEntitySubType:`, item.relatedBusinessEntitySubType);
-        console.log(`Index: ${index}, relatedBusinessEntityStatus:`, item.relatedBusinessEntityStatus);
-        console.log(`Index: ${index}, businessEntityEventType:`, item.businessEntityEventType);
-
-        if (item.relatedBusinessEntitySubType.toUpperCase() == 'NOTC' || item.relatedBusinessEntitySubType.toUpperCase() == 'NOTICE') {
-            //$columns.type.show = false;
-            $columns.deliveryType.show = true;
-            item.deliveryType = getFicoClientCodeDescription(item.additionalCharacteristics.find(item => item.name === 'CLIENT_CODE').value);
-
-            item.comment = formatTextWithLineBreaks(extractCharacteristics(item));
-            debugger;
-            $columns.comment.show = true;
-            $columns.commentLabel.show = false;
-        }
-
-        if (item.relatedBusinessEntitySubType.toUpperCase() == 'CALL-OB') {
-            $columns.dueDate.show = true;
-            item.dueDate = item.relatedBusinessEntityDueDate;
-            $columns.phoneNumber.show = true;
-            item.phoneNumber = item.additionalCharacteristics.find(item => item.name === 'PhoneNumber').value;
-            $columns.reachedCustomer.show = true;
-            item.reachedCustomer = item.additionalCharacteristics.find(item => item.name === 'ReachedCustomer').value;
-            $columns.relatedBusinessEntityAssignedTo.show = true;
-            $columns.relatedBusinessEntityAssignedTeam.show = true;
-        }
-
-        if (item.relatedBusinessEntitySubType.toUpperCase() == 'CALL-IB' || item.relatedBusinessEntitySubType.toUpperCase() == 'SUSPEND' ||
-            item.relatedBusinessEntitySubType.toUpperCase() == 'RESTORE' || item.relatedBusinessEntitySubType.toUpperCase() == 'CEASE') {
-            $columns.dueDate.show = true;
-            item.dueDate = item.relatedBusinessEntityDueDate;
-            $columns.relatedBusinessEntityAssignedTo.show = true;
-            $columns.relatedBusinessEntityAssignedTeam.show = true;
-        }
-        /*if (item.additionalCharacteristics != undefined) {
-            item.additionalCharacteristics.forEach(char => {
-                if (char.name === 'cssRequest') {
-                    const emails = [];
-                    debugger;
-                    Object.keys(char.value).forEach(key => {
-                        if (key.startsWith("Contact_") && key.endsWith("_Email")) {
-                            console.log(`${key}:`, char.value[key]);
-                            emails.push(char.value[key]); // Collect emails
-                        }
-                    });
-                    if (emails.length > 0) {
-                        console.log("Concatenated Emails:\n" + emails.join("\n"));
-                        $columns.email.show = true;
-                        item.email = emails.join("\n");
-                    }
-                }
-                if (char.name === 'cssResponse') {
-                    debugger;
-                    console.log("Message:", char.value.message);
-                }
-                if (char.name === 'EmailAddress') {
-                    //                if (item.relatedBusinessEntitySubType === 'EM-IN' && char.name === 'EmailAddress') {
-                    debugger;
-                    console.log("Email Address:", char.value);
-                    $columns.email.show = true;
-                    item.email = char.value; // + ' ' + index;
-                }
-                if (item.relatedBusinessEntitySubType === 'CALL-OB' && char.name === 'PhoneNumber') {
-                    debugger;
-                    console.log("Phone Number:", char.value);
-                    $columns.phoneNumber.show = true;
-                    item.phoneNumber = char.value; // + ' ' + index;
-                }
-            });
-        }*/
-    });
-    debugger;
-    /* Ensure columns exist
-    if ($columns) {
-        $columns.type.show = !$data.some(row => row.relatedBusinessEntitySubType === 'CALL-OB');
-    } else {
-        console.error("Columns not found.");
-    }*/
-};
 Partial.expandedRowDataTable1_commentOnClick = function($event, widget, row) {
     debugger;
     App.Variables.fullComment.dataSet.dataValue = row.comment;
